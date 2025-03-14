b4789564b9d65edfc2eb280771ce354a0cc5442c
AitorCV17 <2201010047@undc.edu.pe>
2025-03-14 17:55:41 -0500

    env cambiado

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    // Convierte la variable de entorno a número; si no está definida, usa 3000 por defecto.
+    port: Number(process.env.VITE_PORT) || 3000,
+  },
 })
